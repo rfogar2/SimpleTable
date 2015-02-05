0833f23ed211c255b565b3d18ca8b814576d20da
rfogar2 <rfogar2@uic.edu>
2015-02-05 16:14:16 -0600

    Initial Commit

---
+++ SimpleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTable
+//
+//  Created by Ugrad Research on 2/5/15.
+//  Copyright (c) 2015 Ugrad Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTable
+//
+//  Created by Ugrad Research on 2/5/15.
+//  Copyright (c) 2015 Ugrad Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
